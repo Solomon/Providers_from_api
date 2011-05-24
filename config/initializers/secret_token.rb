3ef1db74f3228430546b5e37bb59eccfcb9eb3aa
Solomon Desktop <solomon.kahn@gmail.com>
2011-05-24 12:34:44 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DealmapProviderApp::Application.config.secret_token = 'aba8dd2f0956cc8d3fafad96d3b9ab7a6058be9007b431415c8a348540f0212d0353b44118bb83589c01acd7a887cbd302a7c45eb38479f2b63f3a8c4046fafb'
